56ec019b647ca068a0919215b900cab039f8f0ce
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2024-03-20 15:47:59 -0300

    Preparando para Deploy: WEB

--- erickgym-web/index.js
+++ erickgym-web/index.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://127.0.0.1:8000/exercicios'
+// const API_URL = 'http://127.0.0.1:8000/exercicios'
+const API_URL = 'https://erickgym-api-366.onrender.com/exercicios'
 
 function main(){
 
