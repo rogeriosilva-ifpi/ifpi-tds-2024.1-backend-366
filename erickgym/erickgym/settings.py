fb007e180fea26ff2a5904da3146996f2b5e6f16
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2024-03-20 15:39:57 -0300

    Preparando para Deploy: API

--- erickgym/erickgym/settings.py
+++ erickgym/erickgym/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,10 +26,10 @@ SECRET_KEY = 'django-insecure-g3dv+gdn0jkx%u$!1kgyuwk0&au6k$2)n&x_r1ojwc*do0vncf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED')]
 
 #
-CORS_ALLOWED_ORIGINS = ['http://localhost:5500']
+CORS_ALLOWED_ORIGINS = [config('CORS')]
 
 
 # Application definition
